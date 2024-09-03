015c65bcf8648a293c1c8f63b0421f580e51335d
fillah23 <fillahseptian7l24@gmail.com>
2024-09-03 11:44:59 +0700

    menambah asset,serta membuat login dan management user

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', [LoginController::class, 'login'])->name('login');
+Route::post('actionlogin', [LoginController::class, 'actionlogin'])->name('actionlogin');
+Route::middleware(['auth'])->group(function () {
+Route::resource('users', UserController::class);
+Route::get('/home', [UserController::class, 'index']);
+});
+Route::post('actionlogout', [LoginController::class, 'actionlogout'])->name('actionlogout')->middleware('auth');
